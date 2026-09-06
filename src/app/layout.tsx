432e04fdc328dc79f923a3d03912bcbb93005cca
Oloye <oloyedeadeosun2.0@gmail.com>
2026-09-06 23:46:43 +0100

    feat(analytics): install Google Tag Manager GTM-5P247GPC into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,16 +92,21 @@ export default function RootLayout({
       className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-H0K3LYD0R5"
-          strategy="afterInteractive"
-        />
-        <Script id="ga-gtag" strategy="afterInteractive">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5P247GPC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-H0K3LYD0R5');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5P247GPC');
           `}
         </Script>
         <JsonLd data={organizationSchema()} />
